99e6f4592be15859e4120f4b474f90a357cfe934
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-05 11:03:02 +0530

    Initial commit

---
+++ back-end/src/main/java/lk/ijse/dep10/gallery/api/ImageController.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep10.gallery.api;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+
+}
